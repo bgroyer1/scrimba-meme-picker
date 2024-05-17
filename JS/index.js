c6c4b821b20883bf2598cd845ae50bb7a4c5986b
Braedan Royer <royerbraedan@gmail.com>
2024-05-17 16:39:00 -0700

    fixed the issue with the module by adding a package.json file, created the for of loop

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,24 @@
-import { catsData } from "./catsData.js" 
+import { catsData } from "./catsData.js"
 
 function getEmotionsArray(cats) {
-    const emotionsArray = []
-    for (let cat of cats) {
-        for (let emotion of cat.emotionTags) {
-            emotionsArray.push(emotion)
-        }
-    }
-    return emotionsArray
-    console.log(emotionsArray);
+	const emotionsArray = []
+	for (let cat of cats) {
+		for (let emotion of cat.emotionTags) {
+			emotionsArray.push(emotion)
+		}
+	}
+	return emotionsArray
 }
 
-getEmotionsArray(catsData)
+console.log(getEmotionsArray(catsData))
+
+/*
+function getEmotionsArray2(cats) {
+    const emotionsArray = []
+    for (let i = 0; i < cats.length; i ++) {
+        for (let j = 0; j < cats[i].emotionTags.length; j ++) {
+            emotionsArray.push(cats[i].emotionTags[j])
+        }
+    }
+}
+*/
