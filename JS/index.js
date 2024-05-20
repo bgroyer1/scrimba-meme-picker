8a0bf0931f2b5c70bb7305ccb0bbb700f8abad92
Braedan Royer <royerbraedan@gmail.com>
2024-05-20 09:40:44 -0700

    updated the getEmotionsarray to have an if statement checking to see if the emotion was already in the array

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,48 @@
 import { catsData } from "./catsData.js"
+const emotionsRadioDiv = document.querySelector("#emotion-radios")
+
+/*the commented line is me experimenting with the Set function(is it a function?)
+feel like it would make the code a lot cleaner but will do it as the course intends as im sure they structured it this way for a reason */
 
 function getEmotionsArray(cats) {
 	const emotionsArray = []
 	for (let cat of cats) {
 		for (let emotion of cat.emotionTags) {
-			emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion)
+            }
 		}
 	}
-	return emotionsArray
+	// let sortedEmotionsArray = [...new Set(emotionsArray)]
+	// return sortedEmotionsArray
+    return emotionsArray
 }
 
-console.log(getEmotionsArray(catsData))
+function renderEmotionRadios(cats) {
+	let radioHTML = ""
+	let emotionsArray = getEmotionsArray(cats)
+	for (let emotion of emotionsArray) {
+		radioHTML += `
+                        <div class="emotion-radios">
+                            <input
+                                type="radio"
+                                name=${emotion}
+                                value=${emotion}
+                                id=${emotion}
+                                >
+                            <label for=${emotion}>${emotion}</label>
+                        </div>
+                        `
+	}
+	emotionsRadioDiv.innerHTML = radioHTML
+}
+
+renderEmotionRadios(catsData)
 
 /*
+this is the original for loop, which the video demonstrated to compare how much simpler the syntax of the for-of loop is
+
+
 function getEmotionsArray2(cats) {
     const emotionsArray = []
     for (let i = 0; i < cats.length; i ++) {
